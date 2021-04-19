ad8d6bd36b7c0eaaf23d48746daf5c40e205c190
HussainOsama <hussain.osama.asd@gmail.com>
2021-04-19 21:49:25 +0300

    Done with Auth without testing it

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { StyleSheet, View, ImageBackground, Text } from "react-native";
-import user from "../stores/user";
 
 // Styling
 
 const Home = () => {
-  console.log(user.users);
-  return <Text>Open up App.js to start working on your app {}!</Text>;
+  return <Text>Open up App.js to start working on your app !</Text>;
 };
 
 export default Home;
